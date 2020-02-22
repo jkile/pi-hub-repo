036ac36436525490d74cb569d75bf82a54d8c7f7
Spencer <spencer.m.birch@gmail.com>
2020-02-22 00:27:17 -0700

    unfinished auth and two pages being served

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let password;
 let user = {userName, password}
 document.getElementById("signIn").addEventListener("click", function(){
     user.userName = document.getElementById("userName").value;
-    document.getElementById("userName").value = "";
+    // document.getElementById("userName").value = "";
     user.password = document.getElementById("password").value;
-    document.getElementById("password").value = "";
+    // document.getElementById("password").value = "";
 
 })
 
