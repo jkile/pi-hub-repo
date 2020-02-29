b227f4af3942a044fe1f6c5dedbe696dff35c8c4
Spencer <spencer.m.birch@gmail.com>
2020-02-29 01:34:47 -0700

    added functions to delete DB oblects by ID --updated with less shitty line breaks

--- web-server/app.js
+++ web-server/app.js
@@ -157,19 +157,7 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send()
     }
 })
-// app.delete('/users/:id', async (req, res) => {
-//     try {
-//         const user = await User.findByIdAndDelete(req.params.id)
 
-//         if (!user) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(user)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
 app.delete('/tasks/:id', async (req, res) => {
     try {
         const task = await Task.findByIdAndDelete(req.params.id)
