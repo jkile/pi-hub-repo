e26f161c2cdac6d72276d2d300ea76415be5a6e2
Spencer <spencer.m.birch@gmail.com>
2020-02-29 01:31:56 -0700

    added functions to delete DB oblects by ID

--- web-server/app.js
+++ web-server/app.js
@@ -84,6 +84,22 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+// user delete call to their ID. No parameters needed. 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 // Creates new task, requires description. will accept 'completed' bool.
 // default is false
 // localhost:3000/tasks
@@ -139,6 +155,32 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(400).send()
+    }
+})
+// app.delete('/users/:id', async (req, res) => {
+//     try {
+//         const user = await User.findByIdAndDelete(req.params.id)
+
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
