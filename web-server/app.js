6c5dfae47d728f90a3f6ba128f41790a9b7109bd
Spencer <spencer.m.birch@gmail.com>
2020-02-28 23:18:15 -0700

    added error handling for user updates

--- web-server/app.js
+++ web-server/app.js
@@ -63,9 +63,16 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-
 // this updates users files
 app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'invalid updates' })
+    }
+
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         if (!user) {
