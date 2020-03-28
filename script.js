2fbe24cfe48980ab385a5a77d5de90f9941c79b8
Jake Kile <jkile9@gmail.com>
2020-03-28 01:42:26 -0700

    formatting

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
 
 let userName;
 let password;
-let user = {userName, password}
+let user = {userName, password};
 document.getElementById("signIn").addEventListener("click", async function(event){
     event.preventDefault();
     user.userName = document.getElementById("userName").value;
     // document.getElementById("userName").value = "";
     user.password = document.getElementById("password").value;
-    
     try{
         await axios.post("/users/login", {
             email: user.userName,
             password: user.password
         });
-        await axios.get("/home");
     } catch (e){
         console.log(e);
     }
-    
 
 })
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -23,8 +23,6 @@ router.post('/users/login', async (req, res) => {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
         res.send({ user: user, token })
-        res
-   
     } catch (e) {
         res.status(400).send()
     }
