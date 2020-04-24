b0d959fca24c5eeb9cf7433c5cdbf78bf118834a
Jake Kile <jkile9@gmail.com>
2020-04-23 18:54:06 -0700

    updated login redirect

--- client/src/components/LoginCard/LoginCard.js
+++ client/src/components/LoginCard/LoginCard.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
 import styles from "./LoginCard.module.scss";
+import { Redirect } from 'react-router';
 import axios from "axios";
 
 export default function LoginCard() {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -16,22 +18,29 @@ export default function LoginCard() {
         }
         try {
             await axios.post("/users/login", user);
-            window.location.assign('/home');
+            setIsLoggedIn(true);
         } catch (e) {
             console.log(e);
         }
     }
 
-    return (
-        <div className={styles.loginCard}>
-            <h1 className={styles.loginHeader}>Login</h1>
-            <form onSubmit={handleLogin}>
-                <TextInput type="text" placeholder="Username" value={userName} onChange={e => setUserName(e.target.value)} />
-                <TextInput type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-                <div className={styles.loginButton}>
-                    <Button onClick={handleLogin} text="Login"/>
-                </div>
-            </form>
-        </div>
-    )
+    if (isLoggedIn) {
+        return <Redirect to="/home" />
+    } else {
+        return (
+            <div className={styles.loginCard}>
+                <h1 className={styles.loginHeader}>Login</h1>
+                <form onSubmit={handleLogin}>
+                    <TextInput type="text" placeholder="Username" value={userName} onChange={e => setUserName(e.target.value)} />
+                    <TextInput type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <div className={styles.loginButton}>
+                        <Button onClick={handleLogin} text="Login" />
+                    </div>
+                </form>
+            </div>
+
+        )
+    }
+
+
 }
